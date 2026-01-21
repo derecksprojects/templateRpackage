01d601ccc66f351287ed0b2b9698ef4af672aba0
dereckmezquita <dereck@mezquita.io>
2026-01-21 09:21:58 -0600

    Spellings.

--- scripts/BUILD.sh
+++ scripts/BUILD.sh
@@ -19,14 +19,14 @@ else
 fi
 SCRIPT_NAME=$(basename "$0")
 
-# Colors for output
+# Colours for output
 if [[ -t 1 ]]; then
     RED='\033[0;31m'
     GREEN='\033[0;32m'
     YELLOW='\033[1;33m'
     BLUE='\033[0;34m'
     BOLD='\033[1m'
-    NC='\033[0m' # No Color
+    NC='\033[0m' # No Colour
 else
     RED=''
     GREEN=''
